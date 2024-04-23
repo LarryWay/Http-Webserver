8d6662d201bf615a9b32331b37e1052b45c7ff5a
Larry Wei <larryway15@gmail.com>
2024-04-22 22:22:15 -0400

    readded bytes constructor

--- HTTPResponse.java
+++ HTTPResponse.java
@@ -27,6 +27,13 @@ public class HTTPResponse {
     }
 
 
+    public HTTPResponse(byte[] rawBytes){
+        contentBytes = rawBytes;
+        content = String.valueOf(contentBytes);
+        contentBytesLength = contentBytes.length;
+        status = StatusCode.OK_200;
+    }
+
 
 
     public HTTPResponse(String rawText){
@@ -71,12 +78,37 @@ public class HTTPResponse {
     }
 
 
+    public byte[] toBytes(boolean useRawBytes){
+        StringBuilder builder = new StringBuilder();
+        builder.append(VERSION);
+        builder.append(StatusCode.statusToString(status));
+        builder.append("\r\n");
+        builder.append("Content Length: ");
+        builder.append(Integer.toString(contentBytesLength));
+        builder.append("\r\n\r\n");
+
+        byte[] headerBytes;
+
+        try{
+            headerBytes = builder.toString().getBytes(CHARSET);
+
+            byte[] combinedBytes = new byte[contentBytes.length + headerBytes.length];
+            System.arraycopy(headerBytes, 0, combinedBytes, 0, headerBytes.length);
+            System.arraycopy(contentBytes, 0, combinedBytes, headerBytes.length, contentBytesLength);
+            return combinedBytes;
+        }catch(Exception e){
+            e.printStackTrace();
+            return new byte[0];
+        }
+
+    }
+
+
 
 }
 
 
 /* TODO
- * - Add content-type affiliation
  * - fix the toBytes scheme
  * 
  */
